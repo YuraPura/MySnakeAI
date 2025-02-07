18b4129a6835bb749a721842709ddf4367eafb10
Yura <za07aaaa.y@gmail.com>
2025-02-08 02:54:56 +0300

    crazy ficha

--- move.js
+++ move.js
@@ -31,46 +31,53 @@ export default function getMove(gameState) {
     const boardWidth = gameState.board.width;
     const boardHeight = gameState.board.height;
 
-    // Защита от выхода с поля
     if (myHead.x + 1 >= boardWidth) {
-        isMoveSafe.right = false; // Нельзя в право
+        isMoveSafe.right = false;
     } else if (myHead.x - 1 < 0) {
-        isMoveSafe.left = false; // нельзя в лево
+        isMoveSafe.left = false;
     } else if (myHead.y + 1 >= boardHeight) {
-        isMoveSafe.up = false; // нельзя вверх
-    } else if (myHead.y - 1 >= 0) {
-        isMoveSafe.down = false; // нельзя down
+        isMoveSafe.up = false;
+    } else if (myHead.y - 1 < 0) {
+        isMoveSafe.down = false;
     }
 
     // Улучшение 2 - Защити своего червячка от столкновения со своим телом
-    const MyBody = gameState.you.body.slice(1); // пропуск головы
-    for (const segment.x === myHead) {
-        if else (segment.x === myHead.x + 1 && segment.y === myHead.y) {
-
-        } if else (segment.x === myHead.x - 1 && segment.y === myHead.y) {
-
-        } if else (segment.x === myHead.x && segment.y === myHead.y + 1) {
-
-        } if else (segment.x === myHead.x && segment.y === myHead.y - 1) {
-            
+    const myBody = gameState.you.body.slice(1); // пропускаем голову
+    for (const segment of myBody) {
+        if (segment.x === myHead.x + 1 && segment.y === myHead.y) {
+            isMoveSafe.right = false;
+        } else if (segment.x === myHead.x - 1 && segment.y === myHead.y) {
+            isMoveSafe.left = false;
+        } else if (segment.x === myHead.x && segment.y === myHead.y + 1) {
+            isMoveSafe.up = false;
+        } else if (segment.x === myHead.x && segment.y === myHead.y - 1) {
+            isMoveSafe.down = false;
         }
     }
 
-
     // Улучшение 3 - Защити своего червячка от столкновения с телами соперников 
-    // opponents = gameState.board.snakes;
-    // TODO
+    const opponents = gameState.board.snakes.filter(snake => snake.id !== gameState.you.id);
+    for (const snake of opponents) {
+        for (const segment of snake.body) {
+            if (segment.x === myHead.x + 1 && segment.y === myHead.y) {
+                isMoveSafe.right = false;
+            } else if (segment.x === myHead.x - 1 && segment.y === myHead.y) {
+                isMoveSafe.left = false;
+            } else if (segment.x === myHead.x && segment.y === myHead.y + 1) {
+                isMoveSafe.up = false;
+            } else if (segment.x === myHead.x && segment.y === myHead.y - 1) {
+                isMoveSafe.down = false;
+            }
+        }
+    }
 
     // Проверка, остались ли безопасные ходы (если нет, то куда угодно)
-
     const safeMoves = Object.keys(isMoveSafe).filter((key) => isMoveSafe[key]);
     if (safeMoves.length == 0) {
         return { move: "down" };
+    } else {
+        // Если безопасные ходы есть – выбирается рандомный
+        const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+        return { move: nextMove };
     }
-
-    // Если безопасные ходы есть ход, из них выбирается рандомный
-
-    const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
-
-    return { move: nextMove };
 }
